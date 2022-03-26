66c7ee027526c8f0cbd7a381f2d9ab43fa5414e7
SenadMesetovic <95579061+SenadMesetovic@users.noreply.github.com>
2022-03-26 08:11:37 +0100

    Connect with mongodb

--- app.js
+++ app.js
@@ -3,6 +3,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
+const mongoose =require("mongoose");
 
 var _ = require("lodash");
 var fp = require("lodash/fp");
@@ -13,12 +14,41 @@ var object =require("lodash/fp/object");
 var at = require("lodash/at");
 var curryN=require("lodash/fp/curryN");
 
+mongoose.connect("mongodb://localhost:27017/blogsDB");
+
+const blogSchema = new mongoose.Schema({
+  title: String,
+  content:String
+});
+
+const Blog = mongoose.model("Blog",blogSchema);
+let defaultBlogId=[];
+
+const blog1 = new Blog({
+  title:"Day 1",
+  content:"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+});
+
+const blog2 = new Blog({
+  title:"Day 2",
+  content:"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+});
+
+const blog3 = new Blog({
+  title:"Day 3",
+  content:"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+});
+
+
+
 
 let posts = [];
 
 const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
 const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
 const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
+
+
 
 const app = express();
 
@@ -28,7 +58,15 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
 app.get("/",(req,res)=>{
-  res.render("home",{hStartingContent:homeStartingContent, posts:posts});
+
+  Blog.find({},(err,result)=>{
+    if(result.length===0){
+      Blog.insertMany([blog1,blog2,blog3],(err, docs)=>{});
+      res.redirect("/");
+    }else{
+      res.render("home",{hStartingContent:homeStartingContent, posts:result});
+    }
+  });
 });
 
 app.get("/about",(req,res)=>{
@@ -44,27 +82,24 @@ app.get("/compose",(req,res)=>{
 });
 
 app.get("/posts/:postName",(req,res)=>{
-  let requestedTitle = _.lowerCase(req.params.postName);
-  for(let i =0; i<posts.length;i++){
-    const storedTitle = posts[i].title;
-    if(_.lowerCase(storedTitle)===requestedTitle){
+  let requestedTitle = req.params.postName;
+  Blog.findOne({title:requestedTitle},(err,result)=>{
+    if(!err){
       res.render("post",{
-        pContent:posts[i].content,
-         pTitle:posts[i].title
+        pContent:result.content,
+         pTitle:result.title
        })
-      break;
     }
-  };
-
+  });
 });
 
 app.post("/compose",(req,res)=>{
-  const post = {
-    title:req.body.postTitle,
-    content : req.body.postText,
-  };
+  const blog = new Blog ({
+    title: req.body.postTitle,
+    content : req.body.postText
+  });
 
-  posts.push(post);
+  blog.save();
   res.redirect("/");
 });
 
